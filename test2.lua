ee82852b95dafcbcfe8a1e3faf75e87e1e5fc881
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2024-11-07 01:18:32 +0400

    update testfiles

--- test2.lua
+++ test2.lua
@@ -2,16 +2,27 @@ for k,v in pairs(ents.FindByClass('splinemesh')) do v:Remove() end
 
 local melon = ents.Create( "splinemesh" ) -- Spawn prop
 if ( !IsValid( melon ) ) then return end -- Safety first
-melon:SetPos( Vector(0,-1000,0) ) -- Set pos where is player looking
+melon:SetPos( Vector(0,-1000,100) ) -- Set pos where is player looking
 melon:SetAngles(Angle(0,0,0))
-melon.CURVE = true
-melon.RADIUS = 600
-melon.ANGLE = -60
+melon.CURVE = false
+melon.LENGTH = 600
 melon:Spawn() -- Instantiate prop
+
+local melon1 = ents.Create( "splinemesh" ) -- Spawn prop
+if ( !IsValid( melon1 ) ) then return end -- Safety first
+local worldEndMatrix = melon.OrigMatrix * melon.endMatrix
+local pos = worldEndMatrix:GetTranslation()
+local ang = worldEndMatrix:GetAngles()
+melon1:SetPos( pos ) -- Set pos where is player looking
+melon1:SetAngles( ang ) -- Set pos where is player looking
+melon1.CURVE = true
+melon1.RADIUS = 800
+melon1.ANGLE = 60
+melon1:Spawn() -- Instantiate prop
 
 local melon2 = ents.Create( "splinemesh" ) -- Spawn prop
 if ( !IsValid( melon2 ) ) then return end -- Safety first
-local worldEndMatrix = melon.OrigMatrix * melon.endMatrix
+local worldEndMatrix = melon1.OrigMatrix * melon1.endMatrix
 local pos = worldEndMatrix:GetTranslation()
 local ang = worldEndMatrix:GetAngles()
 melon2:SetPos( pos ) -- Set pos where is player looking
@@ -29,5 +40,5 @@ melon3:SetPos( pos ) -- Set pos where is player looking
 melon3:SetAngles( ang ) -- Set pos where is player looking
 melon3.CURVE = true
 melon3.RADIUS = 600
-melon3.ANGLE = 45
+melon3.ANGLE = -45
 melon3:Spawn() -- Instantiate prop
