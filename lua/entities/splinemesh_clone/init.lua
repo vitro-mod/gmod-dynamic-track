87dde47dd0a06b410ea5ebb7593a84194d202dc5
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2024-11-08 01:43:44 +0400

    Update init.lua

--- lua/entities/splinemesh_clone/init.lua
+++ lua/entities/splinemesh_clone/init.lua
@@ -1,11 +1,16 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
 include('shared.lua')
 
 function ENT:Initialize()
     
-    if !InfMap then return end
-    self.chunkOffset = InfMap.TextToChunk(self.chunkKey)
-    InfMap.prop_update_chunk(self, self.chunkOffset)
-    self:PhysicsInit(SOLID_VPHYSICS)
+    if InfMap then
+        self.chunkOffset = InfMap.TextToChunk(self.chunkKey)
+        InfMap.prop_update_chunk(self, self.chunkOffset)
+    end
+
+    -- self:SetModel(self.parent:GetModel())
+    -- self:PhysicsInit(SOLID_VPHYSICS)
 
     self.chunkPhysics = {}
 
@@ -20,15 +25,20 @@ function ENT:Initialize()
     end
 
     self:PhysicsInitMultiConvex( self.chunkPhysics )
-    self:GetPhysicsObject():EnableMotion( false )
-    self:GetPhysicsObject():SetMass(500000)
-    self:GetPhysicsObject():AddGameFlag(FVPHYSICS_CONSTRAINT_STATIC)
-    self:GetPhysicsObject():AddGameFlag(FVPHYSICS_NO_SELF_COLLISIONS)
     self:SetSolid(SOLID_VPHYSICS)
     self:SetMoveType(MOVETYPE_NONE)
     self:EnableCustomCollisions(true)
     self:DrawShadow(false)
+    -- self:SetRenderMode(RENDERMODE_NONE)
     self:SetNoDraw(true)
     self:AddSolidFlags(FSOLID_FORCE_WORLD_ALIGNED)
     self:AddFlags(FL_STATICPROP)
+
+    local phys = self:GetPhysicsObject()
+    if IsValid(phys) then
+        phys:EnableMotion( false )
+        phys:SetMass(50000)
+        phys:AddGameFlag(FVPHYSICS_CONSTRAINT_STATIC)
+        phys:AddGameFlag(FVPHYSICS_NO_SELF_COLLISIONS)
+    end
 end
