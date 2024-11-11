59bb64af9dfadd7c55468275938ef4352a5ea743
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2024-11-11 21:29:54 +0400

    gpt refactor of bb

--- lua/autorun/sh_bbox.lua
+++ lua/autorun/sh_bbox.lua
@@ -4,24 +4,23 @@ SplineMesh = SplineMesh || {}
 --https://wiki.facepunch.com/gmod/util.GetModelMeshes
 --@param mesh The mesh table like returned by util.GetModelMeshes
 SplineMesh.GetBoundingBox = function(mesh)
-    local minX, minY, minZ = mesh.verticies[1].pos:Unpack()
-    local maxX, maxY, maxZ = mesh.verticies[1].pos:Unpack()
+    local firstPos = mesh.verticies[1].pos
+    local minX, minY, minZ = firstPos:Unpack()
+    local maxX, maxY, maxZ = minX, minY, minZ
 
+    for k, v in pairs(mesh.verticies) do
+        local x, y, z = v.pos:Unpack()
 
-    for k,v in pairs (mesh.verticies) do
-        if v.pos.x < minX then minX = v.pos.x end
-        if v.pos.y < minY then minY = v.pos.y end
-        if v.pos.z < minZ then minZ = v.pos.z end
-        
-        if v.pos.x > maxX then maxX = v.pos.x end
-        if v.pos.y > maxY then maxY = v.pos.y end
-        if v.pos.z > maxZ then maxZ = v.pos.z end
+        if x < minX then minX = x end
+        if y < minY then minY = y end
+        if z < minZ then minZ = z end
+
+        if x > maxX then maxX = x end
+        if y > maxY then maxY = y end
+        if z > maxZ then maxZ = z end
     end
 
-    local min = Vector(minX, minY, minZ)
-    local max = Vector(maxX, maxY, maxZ)
-
-    return min, max
+    return Vector(minX, minY, minZ), Vector(maxX, maxY, maxZ)
 end
 
 SplineMesh.RotateXY = function(mesh)
