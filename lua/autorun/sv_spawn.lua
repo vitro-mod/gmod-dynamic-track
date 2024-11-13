a5daea8f7b92254cef6a3dc892729753a23c3cbf
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2024-11-13 14:15:29 +0400

    spawn function
    close #8

---
+++ lua/autorun/sv_spawn.lua
@@ -0,0 +1,28 @@
+SplineMesh.Spawn = function(pos, ang, plan, profile)
+    local splinemeshes = {}
+
+    for k,planElement in pairs(plan) do
+
+        splinemeshes[k] = ents.Create( "splinemesh" ) -- Spawn prop
+        if ( !IsValid( splinemeshes[k] ) ) then return end -- Safety first
+
+        if k > 1 then
+            local worldEndMatrix = splinemeshes[k-1].OrigMatrix * splinemeshes[k-1].endMatrix
+            pos = worldEndMatrix:GetTranslation()
+            ang = worldEndMatrix:GetAngles()
+
+            splinemeshes[k].profileStart = splinemeshes[k-1].endDistance
+        end
+
+        splinemeshes[k]:SetPos( pos ) -- Set pos where is player looking
+        splinemeshes[k]:SetAngles( ang ) -- Set pos where is player looking
+
+        for planKey,planValue in pairs(planElement) do
+            splinemeshes[k][planKey] = planValue
+        end
+
+        splinemeshes[k].PROFILE = profile
+
+        splinemeshes[k]:Spawn() -- Instantiate prop
+    end
+end
