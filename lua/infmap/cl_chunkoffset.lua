c04e2965f21eb1cc7d1677a4b86733d666851fc6
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2024-11-22 01:01:49 +0400

    vcollide_wireframe updating

--- lua/infmap/cl_chunkoffset.lua
+++ lua/infmap/cl_chunkoffset.lua
@@ -8,3 +8,35 @@ hook.Add("PropUpdateChunk", "SplineMesh_Render", function(ent, chunk, oldchunk)
     local lpco = LocalPlayer().CHUNK_OFFSET
     SplineMesh.RenderOffset:SetTranslation(lpco * -2 * InfMap.chunk_size)
 end)
+
+hook.Remove("PropUpdateChunk", "SplineMesh_Clone_Render")
+hook.Add("PropUpdateChunk", "SplineMesh_Clone_Render", function(ent, chunk, oldchunk)
+    if ent != LocalPlayer() then return end
+ 
+    local lpco = InfMap.ChunkToText(LocalPlayer().CHUNK_OFFSET)
+
+    local function update_vcollide_wireframe()
+        if GetConVar('vcollide_wireframe'):GetBool() then
+            RunConsoleCommand('vcollide_wireframe', '0')
+            RunConsoleCommand('vcollide_wireframe', '1')
+        end
+    end
+
+    for k,v in pairs(ents.FindByClass('splinemesh_clone')) do
+        if not IsValid(v) then continue end
+
+        local co = InfMap.ChunkToText(v.CHUNK_OFFSET)
+
+        -- print(v, lpco, co)
+        if lpco ~= co then
+            if v:GetRenderMode() ~= RENDERMODE_NONE then
+                v._OldRenderMode = v:GetRenderMode()
+            end
+            v:SetRenderMode(RENDERMODE_NONE)
+            update_vcollide_wireframe()
+        elseif v._OldRenderMode then
+            v:SetRenderMode(v._OldRenderMode)
+            update_vcollide_wireframe()
+        end
+    end
+end)
