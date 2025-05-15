b81cffa4fa3ba8a4f11585b63cac5af6d5c4b05e
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2025-05-16 01:58:55 +0200

    Update sv_splinemesh.lua

--- lua/autorun/sv_splinemesh.lua
+++ lua/autorun/sv_splinemesh.lua
@@ -1,7 +1,9 @@
+hook.Remove( "PhysgunPickup", "SplineMeshRestrictPhysGun")
 hook.Add( "PhysgunPickup", "SplineMeshRestrictPhysGun", function( ply, ent )
 	if ent:GetClass() == 'splinemesh' then return false end
 	if ent:GetClass() == 'splinemesh_static' then return false end
 	if ent:GetClass() == 'splinemesh_clone' then return false end
+	if ent:GetClass() == 'prop_physics' and ent.IsSplineMeshStatic then return false end
 end )
 
 SplineMesh = SplineMesh or {}
